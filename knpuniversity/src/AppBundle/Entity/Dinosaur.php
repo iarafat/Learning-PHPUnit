e7b279291043e7f52abee0b580a9a04e4e3a5767
iarafat <miyasinarafat@gmail.com>
2020-05-14 19:31:56 +0600

    tested Dinosaur class getLength and setLength methods

---
+++ knpuniversity/src/AppBundle/Entity/Dinosaur.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Dinosaur
+{
+    private $length = 0;
+
+    public function getLength(): int 
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length)
+    {
+        $this->length = $length;
+    }
+}
