a058e469097053b32e23e911d8c977de0a0e9af1
iarafat <miyasinarafat@gmail.com>
2020-05-14 19:54:47 +0600

    tested Dinosaur specification

--- knpuniversity/src/AppBundle/Entity/Dinosaur.php
+++ knpuniversity/src/AppBundle/Entity/Dinosaur.php
@@ -5,6 +5,19 @@ namespace AppBundle\Entity;
 class Dinosaur
 {
     private $length = 0;
+    private $genus;
+    private $isCarnivorous;
+
+    /**
+     * Dinosaur constructor.
+     * @param string $genus
+     * @param bool $isCarnivorous
+     */
+    public function __construct(string $genus = 'Unknown', bool $isCarnivorous = false)
+    {
+        $this->genus = $genus;
+        $this->isCarnivorous = $isCarnivorous;
+    }
 
     public function getLength(): int 
     {
@@ -14,5 +27,15 @@ class Dinosaur
     public function setLength(int $length)
     {
         $this->length = $length;
+    }
+
+    public function getSpecification(): string
+    {
+        return sprintf(
+            'The %s %scarnivorous dinosaur is %d meters long',
+            $this->genus,
+            $this->isCarnivorous ? '' : 'non-',
+            $this->length
+        );
     }
 }
